29868eb7a3a39c122a1fbaac8241ca44259a3a3b
Matteo Soresini <matteo.soresini@hotmail.it>
2025-07-18 17:52:50 +0200

    feature(waller-connect): wallet with modal popup connected

--- apps/mobile/app/(tabs)/_layout.tsx
+++ apps/mobile/app/(tabs)/_layout.tsx
@@ -32,6 +32,15 @@ export default function TabsLayout() {
           ),
         }}
       />
+      <Tabs.Screen
+        name="wallet"
+        options={{
+          title: "More",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="wallet-outline" size={size} color={color} />
+          ),
+        }}
+      />
     </Tabs>
   );
 }
---
+++ apps/mobile/constants/variables.ts
@@ -0,0 +1,1 @@
+export const projectId = "36cc42754ea30125852b75308c841044";
