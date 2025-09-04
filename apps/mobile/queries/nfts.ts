0047b88abe2c5a2a9ef4c5c234e90f4a1c28eb04
Matteo Soresini <matteo.soresini@hotmail.it>
2025-09-04 17:34:14 +0200

    feature(go): code cleaned

--- apps/mobile/queries/nfts.ts
+++ apps/mobile/queries/nfts.ts
@@ -1,12 +1,8 @@
-import type { nftCategories } from "@/constants/nfts";
 import { urlEndpoints } from "@/constants/urls";
 import type { AlchemyNFT } from "@/types/nft-card";
 import { axiosGet } from "@/utils/api";
 
-export async function getExploreNFTs(
-  address: string,
-  category: keyof typeof nftCategories,
-) {
+export async function getExploreNFTs(address: string, category: string) {
   return axiosGet<{ owned: AlchemyNFT[]; trending: AlchemyNFT[] }>(
     urlEndpoints.getNFTs,
     {
